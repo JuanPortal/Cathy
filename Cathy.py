a6c1ce1eae2387f1669ff197b4d7a46dbc74c3e1
Juan Portal <78442505+JuanPortal@users.noreply.github.com>
2021-04-22 22:34:04 -0500

    Update Cathy.py

--- Cathy.py
+++ Cathy.py
@@ -121,77 +121,98 @@ async def audio(ctx, *args):
     audio_excita = ["porque", "excita", "tanto"]
     audio_segovia = ["mama", "puta"]
 
-    channel = ctx.message.author.voice.channel
-    vc = await channel.connect()
-
     if any(word in arreglar(str(args)).lower() for word in audio_tilin):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Tilín.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_cardi):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Cardi.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_cuack):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Cuack.mp3"))
         time.sleep(1)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_gaa):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Gaa.mp3"))
         time.sleep(4)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_lapo):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Lapo.mp3"))
         time.sleep(1)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_ohno):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Oh ño.mp3"))
         time.sleep(1)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_papi):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Papi, cáchame.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_oidos):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Oídos.mp3"))
         time.sleep(4)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_sagasti):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/SaGAAAAAAAsti.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_auron):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Miedo.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_chupetin):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Chupetín.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
 
     elif any(word in arreglar(str(args)).lower() for word in audio_nooo):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         num = random.randint(1, 3)
         vc.play(discord.FFmpegPCMAudio(f"audios/Noooo{num}.mp3"))
         # print(num)
@@ -200,132 +221,176 @@ async def audio(ctx, *args):
 
     elif any(word in arreglar(str(args)).lower() for word in audio_aumrd):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Au, mrd.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_piero):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Piero.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_ahoraque):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Ahora qué.mp3"))
         time.sleep(1)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_equipo):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Equipo.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_cell):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Cell.mp3"))
         time.sleep(5)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_raiden):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Raiden.mp3"))
         time.sleep(4)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_woody):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Bien pensado, Woody.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_eleccion):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/No tengo elección.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_ricolas):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Ricolás.mp3"))
         time.sleep(7)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_decepcion):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Decepción.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_diablos):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Diablos, señorita.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_nolose):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/No lo sé, tú dime.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_algoandamal):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Algo anda mal.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_manuelgold):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Qué.mp3"))
         time.sleep(1)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_oferton):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Puta, qué ofertón.mp3"))
         time.sleep(3)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_escuchame):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Escúchame, huevón.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_aquaman):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Estás enfermo, Bruce Wayne.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_yamete):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Yamete kudasai.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_imbecil):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Imbécil, te crees muy gracioso.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_chikistrikis):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Aquí no, chikistrikis.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_excita):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Por qué me excita tanto.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
         
     elif any(word in arreglar(str(args)).lower() for word in audio_segovia):
         await ctx.channel.purge(limit=1)
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio("audios/Tu mamá es una puta.mp3"))
         time.sleep(2)
         await ctx.guild.voice_client.disconnect()
